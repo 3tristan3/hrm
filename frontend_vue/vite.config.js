2e7d13436feaaccd1e77dae6019e326b182fc2e7
wizopen <68617486@qq.com>
2026-02-13 16:05:21 +0800

    4

--- frontend_vue/vite.config.js
+++ frontend_vue/vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { fileURLToPath } from "node:url";
 
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: fileURLToPath(new URL("./index.html", import.meta.url)),
+        qr: fileURLToPath(new URL("./qr.html", import.meta.url)),
+      },
+    },
+  },
   server: {
     port: 8080,
   },
